5b8ed78a10fd71e8fe020098439efc2233bc4c9b
Victor Yañez <info@yanezdg.com>
2019-08-30 21:27:56 -0300

    cart functionality

--- script.js
+++ script.js
@@ -1,23 +1,75 @@
-const prs= ['1','2','3','4','5'];
+let cart = [];
 
-fillProds()
+const prods = [
+  {
+    id: 1,
+    name: "Producto 1",
+    description: "Descripcion del producto 1",
+    price: "145"
+  },
+  {
+    id: 7,
+    name: "Producto 11",
+    description: "Descripcion del producto 11",
+    price: "245"
+  },
+  {
+    id: 2,
+    name: "Producto 2",
+    description: "Descripcion del producto 2",
+    price: "195"
+  }
+];
 
-function fillProds(){
-    const card = '<article class="card"><div class="cardImage"></div><div class="cardData"><h3>Product Title</h3><p>Here goes the product description</p><div class="cardFooter"><div class="price">$ 99.00</div><button type="button" class="btn btn-outline-success" onclick="hello(event)">Add</button></div></div></article>'
-    
-    prs.forEach(pr => {
-        let doc = document.getElementById('PrGrid');
-        doc.insertAdjacentHTML('beforeend', card);
-    })
+function fillProds() {
+  prods.forEach((pr, i) => {
+    let card = `<article class="card"><div class="cardImage"></div><div class="cardData"><h3>${pr.name}</h3><p>${pr.description}</p><div class="cardFooter"><div class="price">$ ${pr.price}</div><button type="button" class="btn btn-outline-success" onclick="add(${i})">Add</button></div></div></article>`;
+    let doc = document.getElementById("PrGrid");
+    doc.insertAdjacentHTML("beforeend", card);
+  });
+
+  if (cart.length < 1) {
+    cartIsEmpty();
+  }
 }
 
+function add(id) {
+  const pr = prods[id];
+  cartHasProds();
 
+  const myElement = `<div id="prod_${pr.id}" class="cartProd"><button type="button" class="btn btn-outline-danger btn-sm" onclick="erase(${pr.id})">X</button><span class="cartProdTitle">${pr.name}</span></div>`;
 
-function hello(event) {
-    const myElement= '<div class="prList"><div class="cartProd"><button type="button" class="btn btn-outline-danger btn-sm">X</button><span class="cartProdTitle">Product Here</span></div></div>'
-
-
-    prs.push('hello')
-    console.log('arr > ', event);
-    document.getElementById('ShopItems').insertAdjacentHTML('beforeend', myElement);
+  cart.push(pr);
+  document
+    .getElementById("ShopItems")
+    .insertAdjacentHTML("beforeend", myElement);
 }
+
+function cartIsEmpty() {
+  const myElement = `<div id="emptyCart"><h5>Carrito Vacio</h5></div>`;
+  document
+    .getElementById("ShopItems")
+    .insertAdjacentHTML("beforeend", myElement);
+}
+
+function eraseFromCart(id) {
+  cart = cart.filter(pr => pr.id != id);
+}
+
+function cartHasProds() {
+  let x = document.getElementById("emptyCart");
+  if (x) {
+    x.parentElement.removeChild(x);
+  }
+}
+
+function erase(id) {
+  let eraseMe = document.getElementById("prod_" + id);
+  eraseFromCart(id);
+  document.getElementById("ShopItems").removeChild(eraseMe);
+  if (cart.length < 1) {
+    cartIsEmpty();
+  }
+}
+
+fillProds();
